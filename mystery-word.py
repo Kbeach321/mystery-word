6ce8f9df63e0182f4e8c322343a8f53e14acfa2d
Kbeach321 <iamkevinbeach@gmail.com>
2018-05-17 16:47:58 -0400

    outputcorrect

--- mystery-word.py
+++ mystery-word.py
@@ -1,8 +1,9 @@
 # Mystery-word application #
 # OBJECTIVES: # 3. 1 letter per round "Lives" # 4. Display progress on word
-
+###############################
 import random
-
+#############################
+"""
 def wholegame(guesscounter, statement):
     letter = guessinput
     if letterguesser(letter) is True:
@@ -12,20 +13,18 @@ def wholegame(guesscounter, statement):
         guesscounter -= 1
     return guesscounter, statement
 
-
 def letterguesser(letter):
     if guessinput in randomword:
         return True
     else:
         return False
-
+"""
 def incorrectguess(guessinput):
     pass
     #
 
 def correctguess(guessinput):
     pass
-    #
 
 ############# Gather Random Word ###########################
 
@@ -36,19 +35,19 @@ with open("/usr/share/dict/words") as wholedoc:
     blankspace = "_ " * len(randomword)
 print(randomword) ########### DELTE THIS WHEN COMPLETE ##############
 
-
 ########## TERMS ############
 
 guesscounter = 8
 
 while guesscounter >= 0:
     guessinput = input("Guess a letter! ")
+    output = f"You have {guesscounter} guesses remaining"
     if guessinput in randomword:
         guesscounter += 0
         print("Your word has this many spaces: " + blankspace)
 
     else:
-        print("Nope, guess again")
         guesscounter -= 1
-    output = f"You have {guesscounter} guesses remaining"
+        print("Nope, guess again")
+
     print(output)
